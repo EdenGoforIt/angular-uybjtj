cbd3de63654eb6d1d78a5098f69e53dcd930f35c
EdenGoforIt <79078528+EdenGoforIt@users.noreply.github.com>
2023-05-15 10:31:49 +1200

    3 files modified, 1 added

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule],
   templateUrl: './app.compnent.html',
 })
 export class App {
